c658ff667676c84a99fbfa52010d23a396d46a36
GameScreen <>
2024-04-13 13:53:05 +0300

    lily update

--- board.py
+++ board.py
@@ -8,15 +8,15 @@ class Board:
         self.height = 9
         self.size = 100
         self.board = self.generate()
+        self.lily = pygame.image.load('objects/lily.png')
+        self.lily = pygame.transform.scale(self.lily, (self.size, self.size))
 
     def draw(self, screen):
         for i in range(self.height):
             for j in range(self.width):
-                if self.board[i][j] == 0:
-                    pygame.draw.rect(screen, (0, 0, 255), (self.size * j, self.size * i, self.size, self.size))
                 if self.board[i][j] == 1:
-                    pygame.draw.rect(screen, (255, 0, 0), (self.size * j, self.size * i, self.size, self.size))
+                    screen.blit(self.lily,(self.size*j, self.size*i))
 
     def generate(self):
-        return [list(map(int, ['1'] + list(i)[:-1] + ['2'])) for i in open(f'levels/level_{randint(1, 5)}.txt').readlines()]
+        return [list(map(int, ['2', '2', '2'] + list(i)[:-1] + ['2', '2', '2'])) for i in open(f'levels/level_{randint(1, 7)}.txt').readlines()]
 
