4c157eb3fac75e36abb3b6255a43877f96a34ab8
Maxim133757 <166899133+Maxim133757@users.noreply.github.com>
2024-04-13 14:06:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 import pygame
-from menu import Menu, Settings
+from menu import Menu, Settings, FAQ
 from player import Player
 from board import Board
 
@@ -36,6 +36,7 @@ if __name__ == '__main__':
                         pygame.quit()
                         sys.exit()
             pygame.display.flip()
+
         elif menu_mod == 2:
             if not sett:
                 sett = True
@@ -50,11 +51,24 @@ if __name__ == '__main__':
                     if menu.Button1.pressed(pygame.mouse.get_pos()):
                         pass
                     if menu.Button2.pressed(pygame.mouse.get_pos()):
-                        pass
+                        menu_mod = 3
                     if menu.Button3.pressed(pygame.mouse.get_pos()):
                         menu_mod = 1
                         sett = False
+
             pygame.display.flip()
+        elif menu_mod == 3:
+            faq = FAQ()
+            draw_background(screen, 'backgrounds/FAQ.png')
+            faq.update_display(screen)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if faq.Button1.pressed(pygame.mouse.get_pos()):
+                        menu_mod = 2
+
         else:
             if not fin:
                 board = Board()
